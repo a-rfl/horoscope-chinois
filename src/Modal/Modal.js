cdc522af38575a6bb97c8dd5af4b22084369e348
a-rfl <59601726+a-rfl@users.noreply.github.com>
2021-01-20 17:44:16 +0100

    Modification Modal style

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -4,7 +4,7 @@ const Modal = ({
   <div className={visibile ? 'visible' : 'invisible'}>
     <div className="modal-overlay">
       <div
-        className={`modal pt-2 px-5 pb-3 rounded bg-${color}-600 border-gray-700 border-solid w-1/4`}
+        className={`modal pt-2 px-5 pb-3 rounded bg-${color}-400 border-gray-700 border-solid w-1/4`}
       >
         <h2 className="text-2xl pb-5 mt-4">{title}</h2>
         <div className="m-2 mb-4">{children}</div>
