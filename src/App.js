b3c62e6711d9329821bc37660504f4a85cb8d19f
a-rfl <59601726+a-rfl@users.noreply.github.com>
2021-01-20 10:49:42 +0100

    Modal workshop part 1

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import './App.scss';
 import Card from './Card/Card';
 import FormSign from './Form/Form';
 import signes from './data/data';
+import Modal from './Modal/Modal';
 
 function App() {
+  const attrs = { modalClass: 'main-modal', visible: true };
+
   return (
     <div>
       <h1 className="capitalize text-5xl text-center my-10">
         horoscope chinois
       </h1>
       <FormSign />
-      <div className="App container flex flex-wrap items-stretch justify-center mx-auto mt-5">
+      <div className="App container flex flex-wrap items-stretch justify-center mx-auto my-5">
         {signes.map((signe) => (
           <Card
             signImage={signe.img}
@@ -20,6 +23,15 @@ function App() {
           />
         ))}
       </div>
+      <Modal title="Titre modal 1" color="purple" close {...attrs}>
+        Texte de la <strong>modale</strong>. Suite du texte.
+      </Modal>
+      <Modal title="Titre modal 2" color="red" close {...attrs}>
+        Texte de la <strong>modale</strong>. Suite du texte.
+      </Modal>
+      <Modal title="Titre modal 3" color="blue" close {...attrs}>
+        Texte de la <strong>modale</strong>. Suite du texte.
+      </Modal>
     </div>
   );
 }
--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,6 +1,8 @@
-const Modal = ({ title }) => (
-  <div className="modal">
-    <h2>{title}</h2>
+const Modal = ({ title, children, color }) => (
+  <div className={`modal p-5 pb-3 rounded bg-${color}-300 border-gray-700 border-solid`}>
+    <h2 className="text-2xl pb-5">{title}</h2>
+    <div className="m-2 mb-4">{children}</div>
+    <button className="py-1 px-3 bg-gray-200 rounded btn-modal">Fermer</button>
   </div>
 );
 
